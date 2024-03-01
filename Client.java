c9892c8b55a881e2037aa07946e0b83bbb9229fe
sahaayushioe <sahaayushioe@gmail.com>
2024-03-01 17:45:50 +0545

    java final

--- Client.java
+++ Client.java
@@ -23,19 +23,22 @@ class Client {
     public void startReading(){
         Runnable r1 = () -> {
             System.out.println("Reader started");
+            try{
             while(true){
-                try{
-                    String msg=br.readLine();
-                    if (msg.equals("exit")){
-                        System.out.println("Server terminated the chat");
-                        break;
-                    }
-                    System.out.println("Server : "+msg);
-            } catch (Exception e){
-                e.printStackTrace();
-                } 
-                    
+                        String msg=br.readLine();
+                        if (msg.equals("exit")){
+                            System.out.println("Server terminated the chat");
+                            socket.close();
+                            break;
+                        }
+                        System.out.println("Server : "+msg);
             }
+            } catch (Exception e){
+                //e.printStackTrace();
+                System.out.println("COnnection closed");
+
+            } 
+            System.out.println("Connection closed");
 
         };
         new Thread(r1).start();
@@ -43,16 +46,21 @@ class Client {
     public void startWriting(){
         System.out.println("Writer started");
         Runnable r2 = () -> {
-            while(true){
-                try {
-                    BufferedReader br1=new BufferedReader(new InputStreamReader(System.in));
-                    String content=br1.readLine();
-                    out.println(content);
-                    out.flush();
-                } catch (Exception e) {
-                    e.printStackTrace();
+            try {
+                while(true && !socket.isClosed()){
+
+                        BufferedReader br1=new BufferedReader(new InputStreamReader(System.in));
+                        String content=br1.readLine();
+                        out.println(content);
+                        out.flush();
+
                 }
+            } catch (Exception e) {
+
+                //e.printStackTrace();
+                System.out.println("COnnection closed");
             }
+            System.out.println("COnnection closed");
         };
         new Thread(r2).start();
 
--- Server.java
+++ Server.java
@@ -19,25 +19,29 @@ class Server{
 
             
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.out.println("COnnection closed");
         }
+        System.out.println("COnnection closed");
     }
     public void startReading(){
         Runnable r1 = () -> {
             System.out.println("Reader started");
-            while(true){
-                try{
-                    String msg=br.readLine();
-                    if (msg.equals("exit")){
-                        System.out.println("Client terminated the chat");
-                        break;
-                    }
-                    System.out.println("Client : "+msg);
-            } catch (Exception e){
-                e.printStackTrace();
-                } 
-                    
-            }
+            try{
+                while(true){
+                        String msg=br.readLine();
+                        if (msg.equals("exit")){
+                            System.out.println("Client terminated the chat");
+                            socket.close();
+                            break;
+                        }
+                        System.out.println("Client : "+msg);           
+                }
+            } catch ( Exception e ){
+                //e.printStackTrace();
+                System.out.println("COnnection closed");
+        }
+            System.out.println("COnnection closed");
 
         };
         new Thread(r1).start();
@@ -45,16 +49,17 @@ class Server{
     public void startWriting(){
         System.out.println("Writer started");
         Runnable r2 = () -> {
-            while(true){
-                try {
-                    BufferedReader br1=new BufferedReader(new InputStreamReader(System.in));
-                    String content=br1.readLine();
-                    out.println(content);
-                    out.flush();
-                } catch (Exception e) {
-                    e.printStackTrace();
+            try {
+                while(true && !socket.isClosed()){
+
+                        BufferedReader br1=new BufferedReader(new InputStreamReader(System.in));
+                        String content=br1.readLine();
+                        out.println(content);
+                        out.flush();
                 }
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         };
         new Thread(r2).start();
 
